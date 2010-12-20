248ee1390e04282a37ef808ebed9f1253d2b3fd3
Eric Teubert <ericteubert@googlemail.com>
2010-12-20 20:03:41 +0100

    basic main file generation

--- generate.rb
+++ generate.rb
@@ -1,6 +1,33 @@
 require 'choice'
 require 'erb'
 
+BASEDIR = File.dirname(__FILE__)
+
+class String
+  def slug
+    #strip the string
+    ret = self.strip
+
+    #blow away apostrophes
+    ret.gsub! /['`]/,""
+
+    # @ --> at, and & --> and
+    ret.gsub! /\s*@\s*/, " at "
+    ret.gsub! /\s*&\s*/, " and "
+
+    #replace all non alphanumeric, underscore or periods with underscore
+    ret.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'  
+
+    #convert double underscores to single
+    ret.gsub! /_+/,"_"
+
+    #strip off leading/trailing underscore
+    ret.gsub! /\A[_\.]+|[_\.]+\z/,""
+
+    ret.downcase
+  end
+end
+
 Choice.options do
   
   option :name, :required => true do
@@ -13,6 +40,8 @@ end
 
 name = Choice.choices.name
 
-template = ERB.new File.read('templates/main.rb')
+template = ERB.new File.read(BASEDIR + '/templates/main.rb')
 
-puts template.result
+main = File.new("#{name.slug}.php", "w")
+main.write(template.result)
+main.close
--- templates/main.rb
+++ templates/main.rb
@@ -7,3 +7,52 @@ Author:
 Version: 1.0
 Author URI:
 */
+
+if (!function_exists('<%= name.slug %>_register_frontend_scripts')) {
+  function <%= name.slug %>_register_frontend_scripts()
+  {
+    if (!is_admin()) {
+      // stylesheets
+      wp_register_style('<%= name.slug %>_style',
+        WP_PLUGIN_URL . '/<%= name.slug %>/public/css/<%= name.slug %>.css');
+      wp_enqueue_style('<%= name.slug %>_style');
+      
+      // javascript
+      wp_enqueue_script(
+        '<%= name.slug %>_script',
+          WP_PLUGIN_URL . '/<%= name.slug %>/public/javascript/<%= name.slug %>.js',
+        array('jquery')
+        );
+        
+      // declare the URL to the file that handles the AJAX request (wp-admin/admin-ajax.php)
+      wp_localize_script(
+        '<%= name.slug %>_locale', 
+        '<%= name.slug %>_ajax',
+        array(
+          'url' => admin_url('admin-ajax.php')
+        )
+      );
+    }
+  }
+}
+
+load_theme_textdomain('<%= name.slug %>', dirname(__FILE__) . '/languages/');
+
+add_action('init', '<%= name.slug %>_register_frontend_scripts');
+
+// load and invoke controllers
+require_once dirname(__FILE__) . '/app/controllers/ApplicationController.php';
+
+// add your own controller requires here and then instanciate them anonymously
+// e.g.:
+// new FooController();
+
+if (!function_exists('<%= name.slug %>_add_default_view_directory')) {
+  function <%= name.slug %>_add_default_view_directory($directories)
+  {
+    $child_theme_view_dir = dirname(__FILE__) . '/app/views/';
+    array_unshift($directories, $child_theme_view_dir);
+    return $directories;
+  }
+}
+add_filter('app-view-directories', '<%= name.slug %>_add_default_view_directory');
